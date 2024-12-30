584533cc0476488147e180aeb05a6555451b09d8
SymonBezerra <simaobezerratranslate@gmail.com>
2024-12-30 11:14:09 -0300

    implemented string messages

--- master.py
+++ master.py
@@ -2,6 +2,8 @@ from collections import deque
 import json
 import socket
 
+from message import Message
+
 class Master:
     def __init__(self, ip, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,14 +15,15 @@ class Master:
         self.threads = list()
 
     def recv(self):
-        data, _ = self.socket.recvfrom(1024)
-        return json.loads(data.decode())
+        data, addr = self.socket.recvfrom(1024)
+        # return json.loads(data.decode())
+        return Message(data[:20].strip(), data[20:]), addr
 
     def send(self, msg, ip, port):
         # `send` awaits for a confirmation message
-        self.socket.sendto(json.dumps(msg).encode(), (ip, int(port)))
-        answer, address = self.socket.recvfrom(1024)
-        return json.loads(answer.decode()), address
+        self.socket.sendto(msg.build(), (ip, int(port)))
+        data, _ = self.socket.recvfrom(1024)
+        return Message(data[:20].strip(), data[20:].strip())
 
     def __get_partition(self, node):
         # hash-based partitioning
@@ -33,43 +36,53 @@ class Master:
     def add_node(self, node):
         ip, port = self.__get_partition(node)
         self.nodes[node] = (ip, port)
-        return self.send({
-            'type': 'add_node',
-            'node': node
-        }, ip, port)
+        return self.send(Message(b'ADD_NODE', node.encode()), ip, port)
 
     def add_edge(self, n1, n2, weight=1):
-        return self.send({
-            'type': 'add_edge',
-            'node1': n1,
-            'node2': n2,
-            'weight': weight
-        }, self.nodes[n1][0], self.nodes[n1][1])
+        # return self.send({
+        #     'type': 'add_edge',
+        #     'node1': n1,
+        #     'node2': n2,
+        #     'weight': weight
+        # }, self.nodes[n1][0], self.nodes[n1][1])
+        return self.send(Message(b'ADD_EDGE', f'{n1} {n2} {weight}'.encode()), self.nodes[n1][0], self.nodes[n1][1])
 
     def get_edges(self, node, **kwargs):
         edges = []
+        # if node not in self.nodes:
+        #     return None
+        # self.socket.sendto(json.dumps({
+        #     'type': 'get_edges',
+        #     'node': node,
+        #     'bfs': kwargs.get('bfs'),
+        #     'dfs': kwargs.get('dfs')
+        # }).encode(), (self.nodes[node][0], self.nodes[node][1]))
+        # while True:
+        #     data, addr = self.socket.recvfrom(1024)
+        #     self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
+        #     if json.loads(data).get('status') == 'done':
+        #         break
+        #     edges.append(json.loads(data))
         if node not in self.nodes:
             return None
-        self.socket.sendto(json.dumps({
-            'type': 'get_edges',
-            'node': node,
-            'bfs': kwargs.get('bfs'),
-            'dfs': kwargs.get('dfs')
-        }).encode(), (self.nodes[node][0], self.nodes[node][1]))
+        dfs = kwargs.get('dfs')
+        bfs = kwargs.get('bfs')
+        header = b'GET_EDGES'
+        if dfs: header = b'GET_EDGES_DFS'
+        elif bfs: header = b'GET_EDGES_BFS'
+        self.socket.sendto(Message(header, node.encode()).build(), (self.nodes[node][0], self.nodes[node][1]))
         while True:
-            data, addr = self.socket.recvfrom(1024)
-            self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
-            if json.loads(data).get('status') == 'done':
+            msg, addr = self.recv()
+            self.socket.sendto(Message(b'OK', b'').build(), addr)
+            if msg.header == b'DONE':
                 break
-            edges.append(json.loads(data))
+            edges.append(msg.body)
         return edges
 
 
     def bfs(self, root):
         for thread in self.threads:
-            self.send({
-                'type': 'init_bfs'
-            }, *thread)
+            self.send(Message(b'INIT_BFS', b''), *thread)
         
         nodes = deque([root])
         bfs_tree = {root: []}
@@ -80,7 +93,12 @@ class Master:
             
             if edges:
                 bfs_tree[node] = []
-                destinations = [edge['dest'] for edge in edges if edge['dest'] != node]
+                destinations = []
+                for edge in edges:
+                    src, dest, _ = edge.split()
+                    if dest != node:
+                        destinations.append(dest.decode('latin1'))
+                        bfs_tree[node].append(dest.decode('latin1'))
                 for d in destinations:
                     if bfs_tree.get(d) is None:
                         bfs_tree[d] = []
@@ -91,9 +109,7 @@ class Master:
 
     def dfs(self, root):
         for thread in self.threads:
-            self.send({
-                'type': 'init_dfs'
-            }, *thread)
+            self.send(Message(b'INIT_DFS', b''), *thread)
 
         nodes = deque([root])
         dfs_tree = {root: []}
@@ -104,21 +120,15 @@ class Master:
 
             if edges:
                 if dfs_tree.get(node) is None: dfs_tree[node] = []
-                destinations = [edge['dest'] for edge in edges if edge['dest'] != node]
+                destinations = [edge.split()[1] for edge in edges if edge.split()[2] != node]
                 for d in destinations:
-                    if dfs_tree.get(d) is None:
-                        dfs_tree[d] = []
-                        nodes.append(node)
-                        nodes.append(d)
-                        dfs_tree[node].append(d)
-                        self.send({
-                            'type': 'visit_node',
-                            'node': d
-                        }, *self.nodes[d])
-                        self.send({
-                            'type': 'visit_node',
-                            'node': node
-                        }, *self.nodes[node])
+                    if dfs_tree.get(d.decode()) is None:
+                        dfs_tree[d.decode()] = []
+                        nodes.append(node.decode())
+                        nodes.append(d.decode())
+                        dfs_tree[node].append(d.decode())
+                        self.send(Message(b'VISIT_NODE', d), *self.nodes[d])
+                        self.send(Message(b'VISIT_NODE', node), *self.nodes[node])
                         break
 
         return dfs_tree
--- thread.py
+++ thread.py
@@ -2,6 +2,7 @@ import json
 import socket
 
 from graph import Edge
+from message import Message
 
 class Thread:
     def __init__(self, ip, port):
@@ -15,53 +16,91 @@ class Thread:
 
     def recv(self):
         data, addr = self.socket.recvfrom(1024)
-        msg = json.loads(data.decode())
-        if msg.get('type') == 'add_node':
-            node = msg.get('node')
-            self.edges[node] = []
-            self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
-        elif msg.get('type') == 'add_edge':
-            n1, n2 = msg.get('node1'), msg.get('node2')
-            weight = msg.get('weight', 1)
-            self.edges[n1].append(Edge(n1, n2, weight))
-            self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
-        elif msg.get('type') == 'init_bfs' or msg.get('type') == 'init_dfs':
+        # msg = json.loads(data.decode())
+        # if msg.get('type') == 'add_node':
+        #     node = msg.get('node')
+        #     self.edges[node] = []
+        #     self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
+        # elif msg.get('type') == 'add_edge':
+        #     n1, n2 = msg.get('node1'), msg.get('node2')
+        #     weight = msg.get('weight', 1)
+        #     self.edges[n1].append(Edge(n1, n2, weight))
+        #     self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
+        # elif msg.get('type') == 'init_bfs' or msg.get('type') == 'init_dfs':
+        #     self.buffer['visited'] = set()
+        #     self.buffer['nodes_added'] = set()
+        #     self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
+        # elif msg.get('type') == 'bfs' or msg.get('type') == 'dfs':
+        #     node = msg.get('node')
+        #     if node in self.buffer['visited']:
+        #         self.socket.sendto(json.dumps({'status': 'visited'}).encode(), addr)
+        #     else:
+        #         self.buffer['visited'].add(node)
+        #         self.socket.sendto(json.dumps({'status': 'not_visited'}).encode(), addr)
+        # elif msg.get('type') == 'get_edges':
+        #     node = msg.get('node')
+        #     for edge in self.edges[node]:
+        #         if msg.get('bfs') and (edge.dest in self.buffer['nodes_added']): continue
+        #         elif msg.get('dfs') and (edge.dest in self.buffer['visited']): continue
+        #         elif msg.get('bfs'): self.buffer['nodes_added'].add(edge.dest)
+        #         self.socket.sendto(json.dumps({
+        #             'src': edge.src,
+        #             'dest': edge.dest,
+        #             'weight': edge.weight,
+        #             'attr': edge.attr
+        #         }).encode(), addr)
+        #         data, _ = self.socket.recvfrom(1024)
+        #         if json.loads(data.decode()).get('status') != 'ok':
+        #             break
+        #     if msg.get('bfs'):
+        #         self.buffer['visited'].add(node)
+        #         self.buffer['nodes_added'].add(node)
+        #     self.socket.sendto(json.dumps({'status': 'done'}).encode(), addr)
+        # elif msg.get('type') == 'visit_node':
+        #     node = msg.get('node')
+        #     self.buffer['visited'].add(node)
+        #     self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
+        msg = Message(data[:20].strip(), data[20:].strip())
+        if msg.header == b'ADD_NODE':
+            node = msg.body
+            self.edges[node.decode()] = []
+            self.socket.sendto(Message(b'OK', b'').build(), addr)
+        elif msg.header == b'ADD_EDGE':
+            n1, n2, weight = msg.body.decode().split()
+            self.edges[n1].append(Edge(n1, n2, int(weight)))
+            self.socket.sendto(Message(b'OK', b'').build(), addr)
+        elif msg.header == b'INIT_BFS' or msg.header == b'INIT_DFS':
             self.buffer['visited'] = set()
             self.buffer['nodes_added'] = set()
-            self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
-        elif msg.get('type') == 'bfs' or msg.get('type') == 'dfs':
-            node = msg.get('node')
+            self.socket.sendto(Message(b'OK', b'').build(), addr)
+        elif msg.header == b'BFS' or msg.header == b'DFS':
+            node = msg.body
             if node in self.buffer['visited']:
-                self.socket.sendto(json.dumps({'status': 'visited'}).encode(), addr)
+                self.socket.sendto(Message(b'VISITED', b'').build(), addr)
             else:
                 self.buffer['visited'].add(node)
-                self.socket.sendto(json.dumps({'status': 'not_visited'}).encode(), addr)
-        elif msg.get('type') == 'get_edges':
-            node = msg.get('node')
-            for edge in self.edges[node]:
-                if msg.get('bfs') and (edge.dest in self.buffer['nodes_added']): continue
-                elif msg.get('dfs') and (edge.dest in self.buffer['visited']): continue
-                elif msg.get('bfs'): self.buffer['nodes_added'].add(edge.dest)
-                self.socket.sendto(json.dumps({
-                    'src': edge.src,
-                    'dest': edge.dest,
-                    'weight': edge.weight,
-                    'attr': edge.attr
-                }).encode(), addr)
-                data, _ = self.socket.recvfrom(1024)
-                if json.loads(data.decode()).get('status') != 'ok':
+                self.socket.sendto(Message(b'NOT_VISITED', b'').build(), addr)
+        elif msg.header == b'GET_EDGES' or msg.header == b'GET_EDGES_BFS' or msg.header == b'GET_EDGES_DFS':
+            node = msg.body
+            for edge in self.edges[node.decode()]:
+                if msg.header == b'GET_EDGES_BFS' and (edge.dest in self.buffer['nodes_added']): continue
+                elif msg.header == b'GET_EDGES_DFS' and (edge.dest in self.buffer['visited']): continue
+                elif msg.header == b'GET_EDGES_BFS': self.buffer['nodes_added'].add(edge.dest)
+                self.socket.sendto(Message(b'EDGE', f'{edge.src} {edge.dest} {edge.weight}'.encode()).build(), addr)
+                answer, _ = self.socket.recvfrom(1024)
+                if Message(answer[:20].strip(), answer[20:]).header != b'OK':
                     break
-            if msg.get('bfs'):
+            if msg.header == b'GET_EDGES_BFS':
                 self.buffer['visited'].add(node)
                 self.buffer['nodes_added'].add(node)
-            self.socket.sendto(json.dumps({'status': 'done'}).encode(), addr)
-        elif msg.get('type') == 'visit_node':
-            node = msg.get('node')
+            self.socket.sendto(Message(b'DONE', b'').build(), addr)
+        elif msg.header == b'VISIT_NODE':
+            node = msg.body
             self.buffer['visited'].add(node)
-            self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
+            self.socket.sendto(Message('OK', '').build(), addr)
         return data
 
-    def send(self, msg, ip, port):
-        self.socket.sendto(json.dumps(msg).encode(), (ip, int(port)))
+    def send(self, header, body, ip, port):
+        self.socket.sendto(Message(header, body).build(), (ip, int(port)))
         answer, address = self.socket.recvfrom(1024)
-        return json.loads(answer.decode()), address
+        return Message(answer.decode()[:20].strip(), answer.decode()[20:].strip())
