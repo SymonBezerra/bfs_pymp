77dd9a8deaa01e020261df03a645e34e53a18bd6
SymonBezerra <simaobezerratranslate@gmail.com>
2025-01-03 08:55:24 -0300

    removed unused JSON code

--- thread.py
+++ thread.py
@@ -27,50 +27,6 @@ class Thread:
 
     def recv(self):
         data, addr = self.socket.recvfrom(1024)
-        # msg = json.loads(data.decode())
-        # if msg.get('type') == 'add_node':
-        #     node = msg.get('node')
-        #     self.edges[node] = []
-        #     self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
-        # elif msg.get('type') == 'add_edge':
-        #     n1, n2 = msg.get('node1'), msg.get('node2')
-        #     weight = msg.get('weight', 1)
-        #     self.edges[n1].append(Edge(n1, n2, weight))
-        #     self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
-        # elif msg.get('type') == 'init_bfs' or msg.get('type') == 'init_dfs':
-        #     self.cache['visited'] = set()
-        #     self.cache['nodes_added'] = set()
-        #     self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
-        # elif msg.get('type') == 'bfs' or msg.get('type') == 'dfs':
-        #     node = msg.get('node')
-        #     if node in self.cache['visited']:
-        #         self.socket.sendto(json.dumps({'status': 'visited'}).encode(), addr)
-        #     else:
-        #         self.cache['visited'].add(node)
-        #         self.socket.sendto(json.dumps({'status': 'not_visited'}).encode(), addr)
-        # elif msg.get('type') == 'get_edges':
-        #     node = msg.get('node')
-        #     for edge in self.edges[node]:
-        #         if msg.get('bfs') and (edge.dest in self.cache['nodes_added']): continue
-        #         elif msg.get('dfs') and (edge.dest in self.cache['visited']): continue
-        #         elif msg.get('bfs'): self.cache['nodes_added'].add(edge.dest)
-        #         self.socket.sendto(json.dumps({
-        #             'src': edge.src,
-        #             'dest': edge.dest,
-        #             'weight': edge.weight,
-        #             'attr': edge.attr
-        #         }).encode(), addr)
-        #         data, _ = self.socket.recvfrom(1024)
-        #         if json.loads(data.decode()).get('status') != 'ok':
-        #             break
-        #     if msg.get('bfs'):
-        #         self.cache['visited'].add(node)
-        #         self.cache['nodes_added'].add(node)
-        #     self.socket.sendto(json.dumps({'status': 'done'}).encode(), addr)
-        # elif msg.get('type') == 'visit_node':
-        #     node = msg.get('node')
-        #     self.cache['visited'].add(node)
-        #     self.socket.sendto(json.dumps({'status': 'ok'}).encode(), addr)
         msg = Message(data[:20].strip(), data[20:].strip())
         if msg.header == b'ADD_NODE':
             node = msg.body
